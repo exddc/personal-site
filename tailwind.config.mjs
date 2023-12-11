65a057d1f7ee717d84f7d1fe1c1da3386c30b573
Timo Weiss <timo.weiss@protonmail.ch>
2023-12-11 22:11:38 +0100

    Added new font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    'InterVariable',
+                    'Inter',
+                    ...defaultTheme.fontFamily.sans,
+                ],
+            },
+        },
+    },
+    plugins: [],
+};

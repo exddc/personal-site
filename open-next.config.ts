9b4d66ca50b45d5a63e885c6bf2efef4ddebe818
Timo Weiss <timo.weiss@protonmail.ch>
2025-12-31 14:35:42 +0100

    Opennext setup changes

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,17 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import {
+  defineCloudflareConfig,
+  type OpenNextConfig,
+} from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({
+const baseConfig = defineCloudflareConfig({
   incrementalCache: r2IncrementalCache,
 });
+
+const config: OpenNextConfig = {
+  ...baseConfig,
+  buildCommand: "next build",
+};
+
+export default config;
 
